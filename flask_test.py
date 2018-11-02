0fdb5c5e456f3f0579a454d70d2c4e6cda665a7d
Jade Ho <jadeho@Jadede-MacBook-Pro-2.local>
2018-11-02 10:04:10 +0800

    first commit

---
+++ flask_test.py
@@ -0,0 +1,28 @@
+﻿#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 18 13:11:51 2018
+
+@author: jadeho
+"""
+from flask import Flask, jsonify, request
+import logging
+
+app = Flask(__name__)
+
+handler = logging.FileHandler('app.log', encoding='UTF-8')
+logging_format = logging.Formatter(
+            '%(asctime)s - %(levelname)s - %(filename)s - %(funcName)s - %(lineno)s - %(message)s')
+handler.setFormatter(logging_format)
+app.logger.addHandler(handler)
+
+
+@app.route('/get_test' , methods=['GET'])
+def get_test():
+    app.logger.info('訪問')
+    return jsonify('TEST SUESS')
+
+
+if __name__ == '__main__':
+    # MAIN
+    app.run(port=5000)#debug=True
