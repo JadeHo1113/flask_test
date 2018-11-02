270434f99b0c46ad270a1a2429612b353965b717
Jade Ho <jadeho@Jadede-MacBook-Pro-2.local>
2018-11-02 14:07:28 +0800

    update

---
+++ docker_build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build --no-cache -t="fubonlife/bio-tts:latest" -t="sdtwvms00305:5000/fubonlife/bio-tts:latest" .
---
+++ docker_push.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker push sdtwvms00305:5000/fubonlife/bio-tts:latest
